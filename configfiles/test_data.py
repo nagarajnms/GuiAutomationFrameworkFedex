ae814fb9808e8942640092aef2134a513be2c33d
Nagaraj <nagarajnms@gmail.com>
2022-10-14 22:32:57 +0200

    Minor changes to the test_data file.

--- configfiles/test_data.py
+++ configfiles/test_data.py
@@ -3,12 +3,9 @@ user = {
     "password": "Hello@123",
     "username": "Nagaraj "
 }
-
 tracking_ids = {
     "single_item": "1234",
     "multiple_items": ("34324", "5645", "9807")
 }
-
 location = "AMSTELVEEN, NETHERLANDS"
-
 ask_assistant_message = "I need info on charges for courier service"
